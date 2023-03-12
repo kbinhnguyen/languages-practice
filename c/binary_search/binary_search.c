f4c6859a5a9e902eb594500eb61bd1d56615d792
Binh Nguyen <kbinhnguyen.nihongo@gmail.com>
2023-03-12 14:28:40 -0400

    c practice

---
+++ c/binary_search/binary_search.c
@@ -0,0 +1,10 @@
+#include "binary_search.h"
+
+const int *binary_search(int value, const int *arr, size_t length) {
+    if (arr >= arr + length) return NULL;
+
+    const int *mid = arr + (length / 2);
+    if (*mid == value) return mid;
+    else if (*mid > value) return binary_search(value, arr, length / 2);
+    return binary_search(value, (mid + 1), length % 2 == 0 ? length / 2 - 1 : length / 2);
+}
